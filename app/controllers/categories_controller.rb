5d75573deb9a71da39dfed78edf547e150dc1514
Bria222 <techconsultants.bree@gmail.com>
2022-11-16 16:03:38 +0300

    add create new category method and authenticate user on each category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @categories = Category.all
+    @categories = current_user.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  
+
+  private
+
+  def category_params
+    params.require(:new_category).permit(:name, :icon)
   end
 end
