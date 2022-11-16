350831f93bf0926fe0c103926c41875a544f4840
Bria222 <techconsultants.bree@gmail.com>
2022-11-16 13:25:07 +0300

    generate user model

--- db/migrate/20221116101754_create_categories.rb
+++ db/migrate/20221116101754_create_categories.rb
@@ -3,6 +3,8 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :name
       t.string :icon
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :payment, null: false, foreign_key: true
 
       t.timestamps
     end
